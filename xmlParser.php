7e8d970be50ddff4e3bc9035fbad14c2943975b1
Dorian <i.am.eager.for.peace@gmail.com>
2014-08-02 21:03:54 +0900

    XMLパーサを作成。シリアライズしてファイル保存するのでアンシリアライズしてphpの配列に戻すunSerializerも作成

--- xmlParser.php
+++ xmlParser.php
@@ -1,5 +1,21 @@
-<?php 
-  $xml = simplexml_load_file('https://www.google.com/alerts/feeds/05546308791182870999/12557010243017956011');
-  pr($xml);
+<?php
+//コストコのgooglealertを読み込んでシリアライズしてcostco.txtとして保存
+class parseXML {
+  public function loadXML() {
+    $xml = simplexml_load_file('https://www.google.com/alerts/feeds/05546308791182870999/12557010243017956011');
+    foreach($xml->entry as $value) {
+      $result[] = array('title' => (string)$value->title, 
+        'link' => (string)$value->link, 
+        'updated' => (string)$value->updated
+      );
+    }
 
+    $fileAccess = fopen('costco.txt', 'w');
+    fwrite($fileAccess, serialize($result));
+    fclose($fileAccess);
+  }
+}
+
+$parseXML = new parseXML;
+$parseXML->loadXML();
 ?>
