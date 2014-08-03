bcfd80bb487b24ce5662ac31f34f335e1a6385b8
Dorian <i.am.eager.for.peace@gmail.com>
2014-08-03 11:58:56 +0900

    パース先が書かれたCSVファイルを読み取り、指定されたファイル名でシリアライズ化したものを保存する

--- xmlAutoParser.php
+++ xmlAutoParser.php
@@ -1,2 +1,21 @@
 <?php
 //parseList.phpに書かれたパース先を順番に読み取って(配列かして、要素を指定して順番に読み取る)、それぞれの定数の名前のファイルに出力
+
+class parseXML {
+
+  public function loadXML() {
+    //xmlSelectorから渡されたXMLのURLを読み取りパースして、指定の定数値のフィある名で保存する
+    $this->xmlSelector();
+  }
+
+  private function xmlSelector() {
+    //parseList.csvに書かれたパース先を順番に読み取り、loadXMLに渡す
+    $source = fopen('parseList.csv', 'r');
+    while($parseListArray = fgetcsv($source)) {
+      var_dump($parseListArray);
+    }
+  }
+}
+$parseXML = new parseXML;
+$parseXML->loadXML();
+?>
