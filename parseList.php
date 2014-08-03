c125d76ce36c3b8f003521d373dfe29f62908ff5
Dorian <i.am.eager.for.peace@gmail.com>
2014-08-03 12:03:05 +0900

    パース先をCSVファイルとして保存するので、constで定数化したパースリストphpを除去

--- parseList.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define("COSTCO", "https://www.google.com/alerts/feeds/05546308791182870999/12557010243017956011");
-define("SHINJUKU", "https://www.google.com/alerts/feeds/05546308791182870999/9998280928482206545");
-define("SEIKEI", "https://www.google.com/alerts/feeds/05546308791182870999/17182401088930987533");
-
-?>
