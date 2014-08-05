03ebf8bb977708b7544cabb1d0e03ea298e5740d
Takahiro ISHIDA <i.am.eager.for.peace@gmail.com>
2014-08-05 22:14:35 +0900

    Just create Not Work correctly

--- xmlAutoParser.php
+++ xmlAutoParser.php
@@ -5,17 +5,29 @@ class parseXML {
 
   public function loadXML() {
     //xmlSelectorから渡されたXMLのURLを読み取りパースして、指定の定数値のフィある名で保存する
-    $this->xmlSelector();
+    $parseScope = $this->xmlSelector();
+    var_dump($parseScope);
   }
 
   private function xmlSelector() {
     //parseList.csvに書かれたパース先を順番に読み取り、loadXMLに渡す
     $source = fopen('parseList.csv', 'r');
-    while($parseListArray = fgetcsv($source)) {
-      var_dump($parseListArray);
+    while ($parseListArray = fgetcsv($source)) {
+      $parseContent = simplexml_load_file($parseListArray[1]);
+
+      foreach ($parseContent->entry as $value) {
+        $result[] = array('title' => (string)$value->title,
+                          'link' => (string)$value->link,
+                          'updated' => (string)$value->updated
+                          );
+        var_dump($result);
+      }
     }
+    //unset($parseListArray);
   }
 }
+
 $parseXML = new parseXML;
 $parseXML->loadXML();
+
 ?>
