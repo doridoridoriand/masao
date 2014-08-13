9d822bbe3aa7c0f3f76b78d4b644fbe104f383de
vagrant <vagrant@precise64.(none)>
2014-08-13 05:12:06 +0000

    php実行時に第一引数にparseResultに格納されているファイル名を指定するとデシリアライズするように修正

--- parseContentDiff.php
+++ parseContentDiff.php
@@ -1,3 +1,23 @@
 <?php
 //xmlAutoParserNewから出力された、シリアライズ化されたパース結果を読み取って今日の日付のものが合ったらそのentryの配列を返す
+
+class compareContent {
+	public function compare($content) {
+		$this->loadContent($content);
+		//parseResultにある
+	}
+
+	private function loadContent($contentName) {
+		//parseResultにあるシリアライズかされた結果を読み取ってデシリアライズする
+		$source = fopen('./parseResult/' . $contentName, 'r');
+		$content = fread($source, filesize('./parseResult/' . $contentName));
+		$phpArray = unserialize($content);
+		fclose($source);
+		var_dump($phpArray);
+	}
+}
+
+$compareContent = new compareContent;
+$compareContent->compare($argv[1]);
+
 ?>
