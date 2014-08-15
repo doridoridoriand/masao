3ce7089080e953d709b04744d3b1b5c74609682f
doridoridoriand <i.am.eager.for.peace@gmail.com>
2014-08-15 03:08:31 +0000

    parseManagerを作成して、xmlAutoParserを実行するように修正

--- xmlAutoParser.php
+++ xmlAutoParser.php
@@ -1,22 +1,40 @@
 <?php
-//parseList.phpに書かれたパース先を順番に読み取って(配列かして、要素を指定して順番に読み取る)、それぞれの定数の名前のファイルに出力
+//parseList.csvに書かれたパース先を順番に読み取って(配列かして、要素を指定して順番に読み取る)、それぞれの定数の名前のファイルに出力
 //第1引数に保存名、第2引数にパース先のURLを指定する形で実行する
 class parseXML {
 
-    public function loadXML() {
-      //readParseURLからパース先を読み取って第1引数の名前で保存する
-      $source = $this->readParseURL();
-      //yotta
+	/*パース先のXMLから必要な情報を持ってきてシリアライズ化して保存する
+
+	　　readParseURLからパース先を読み取って第1引数の名前で保存する
+
+	　　@param string パース内容の保存名称　パース先のURL
+	  @return string シリアライズ化したパース内容
+	 */
+    public function loadXML($parseURLName, $parseURL) {
+      	$source = $this->readParseURL($parseURL);
+    
+      	foreach ($source->entry as $value) {
+      		$parseResult[] = array('title'    => (string)$value->title,
+      								'link'    => (string)$value->link,
+      								'updated' => (string)$value->updated
+      								);
+      	}
+      	$fileAccess = fopen('./parseResult/' . $parseURLName, 'w');
+      	fwrite($fileAccess, serialize($parseResult));
+      	fclose($fileAccess);
     }
 
-    private function readParseURL() {
-      // 実行時に指定された第2引数からURLを読み取り、パース先とする
-      return simplexml_load_file($argv[2]);
+    /*実行時に指定された第2引数からURLを読み取る
+
+      第2引数のURLを読み取り、xmlをphpのオブジェクトに変換する
+
+      @param string パース先のURL
+      @return string パース後のオブジェクト
+    */
+    private function readParseURL($parseURL) {
+      return simplexml_load_file($parseURL);
     }
 }
 
-
-$parseXML = new parseXML;
-$parseXML->loadXML();
-
-?>
+//$parseXML = new parseXML;
+//$parseXML->loadXML($argv[1], $argv[2]);
