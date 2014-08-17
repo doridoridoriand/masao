31f9e1f02f3758edc1bd0945f7c7b6c01ee6bea5
doridoridoriand <i.am.eager.for.peace@gmail.com>
2014-08-17 04:03:55 +0000

    viewを作成

--- view/index.php
+++ view/index.php
@@ -5,3 +5,24 @@
    - いつ実行したか
 */
 require('./elements/header.php');
+require('./helpers/parseListViewHelper.php');
+
+echo '
+<br><br>
+<div class="container">
+	<h2 class="sub-header">現在パースしているGoogle Alert一覧</h2>
+	<div class="table-responsive">
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>保存名称</th>
+					<th>rssのURL</th>
+				</tr>
+			</thead>
+			<tbody>';
+				$parseListViewHelper->parseListConverter();
+echo '
+			</tbody>
+		</table>
+	</div>
+</div class="container">';
---
+++ view/parseResult.php
@@ -0,0 +1,19 @@
+<?php
+/* # ミニマム機能
+   - パースした結果の表示
+*/
+
+require('./elements/header.php');
+//sidebar
+echo '
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-3 col-md-2 sidebar">
+			<ul class="nav nav-sidebar">
+			';
+//main content
+echo '
+<br><br>
+<div class="container">
+<h2 class="sub-header">UNKO</h2>
+';
